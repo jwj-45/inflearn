8a0ae3ec1c6e68c97295c889884aecf696c815b3
junwon <gkatn3@naver.com>
2024-11-26 22:06:45 +0900

    Disjoint-Set : Union&Find

--- Section9/Main6.java
+++ Section9/Main6.java
@@ -4,14 +4,31 @@ import java.util.*;
 public class Main6 {
 	
 	static int[] unf;
+	public static int Find(int v) {
+		if(v == unf[v]) {
+			return v;
+		}
+		else {
+			return Find(unf[v]);
+		}
+	}
+	
+	public static void Union(int a, int b) {
+		int fa = Find(a);
+		int fb = Find(b);
+		if(fa != fb) {
+			unf[fa] = fb;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
 		unf = new int[n + 1];
 		for(int i = 1; i <= n; i++) {
 			unf[i] = i;
@@ -26,6 +43,18 @@ public class Main6 {
 		}
 		st = new StringTokenizer(br.readLine());
 		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int fa = Find(a);
+		int fb = Find(b);
 		
+		String ans = "";
+		if(fa == fb) {
+			ans = "YES";
+		}
+		else {
+			ans = "NO";
+		}
+		System.out.println(ans);
 	}
 }
