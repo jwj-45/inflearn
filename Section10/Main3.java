02dd0fe43820838116e5d86c653e44d5387ff435
junwon <gkatn3@naver.com>
2025-02-06 23:05:36 +0900

    section10

--- Section10/Main3.java
+++ Section10/Main3.java
@@ -3,6 +3,25 @@ import java.util.*;
 
 public class Main3 {
 	
+	static int[] dy;
+	public int solution(int[] arr) {
+		int answer = 0;
+		
+		dy = new int[arr.length];
+		dy[0] = 1;
+		for(int i = 1; i < arr.length; i++) {
+			int max = 0;
+			for(int j = i - 1; j >= 0; j--) {
+				if(arr[j] < arr[i] && dy[j] > max){
+					max = dy[j];
+				}
+			}
+			dy[i] = max + 1;
+			answer = Math.max(answer, dy[i]);
+		}
+		return answer;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,6 +34,6 @@ public class Main3 {
 		
 		Main3 main = new Main3();
 		
-		main.solution();
+		System.out.println(main.solution(arr));
 	}
 }
---
+++ Section10/Main6.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main6 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int s = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			
+			
+		}
+	}
+}
