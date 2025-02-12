01c2adc9ae6a33fea4aaaaa967dff4c25e4d4cd8
junwon <gkatn3@naver.com>
2025-02-12 17:27:21 +0900

    section

--- Section10/Main3.java
+++ Section10/Main3.java
@@ -5,14 +5,13 @@ public class Main3 {
 	
 	static int[] dy;
 	public int solution(int[] arr) {
-		int answer = 0;
-		
 		dy = new int[arr.length];
+		int answer = 1;
 		dy[0] = 1;
 		for(int i = 1; i < arr.length; i++) {
 			int max = 0;
 			for(int j = i - 1; j >= 0; j--) {
-				if(arr[j] < arr[i] && dy[j] > max){
+				if(arr[j] < arr[i] && dy[j] > max) {
 					max = dy[j];
 				}
 			}
@@ -25,13 +24,15 @@ public class Main3 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
 		Main3 main = new Main3();
 		
 		System.out.println(main.solution(arr));
